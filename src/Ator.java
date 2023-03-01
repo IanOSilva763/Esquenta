acf7992cbe661f64ccf715b3d0bd42aa1967d593
ian.silva <ian.o.s@hotmail.com>
2023-03-01 20:23:41 -0300

    classes

--- src/Ator.java
+++ src/Ator.java
@@ -1,13 +1,2 @@
 public class Ator extends Pessoa {
-    private String nome;
-
-    @Override
-    public String getNome() {
-        return nome;
-    }
-
-    @Override
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- src/Diretor.java
+++ src/Diretor.java
@@ -1,13 +1,2 @@
 public class Diretor extends Pessoa {
-    private String nome;
-
-    @Override
-    public String getNome() {
-        return nome;
-    }
-
-    @Override
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
